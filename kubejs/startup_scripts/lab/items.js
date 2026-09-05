84dcbe06b4466efa029ee4a543f9a2e8a7a83cfd
ABO47 <abdullah.mo7amed.47@gmail.com>
2026-09-05 14:53:51 +0300

    chore: tweak some things

---
+++ kubejs/startup_scripts/lab/items.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('item', event => {
+    event.create('lab/fiber', 'basic')
+        .texture('kubejs:item/lab/fiber')
+        .displayName('fiber')
+        .rarity('COMMON')
+        ;
+});
+
